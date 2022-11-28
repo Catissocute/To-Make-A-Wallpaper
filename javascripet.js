6acc26f5b71b6d90ac48e1ab8589c5ee0bf3968b
Catissocute <98471871+Catissocute@users.noreply.github.com>
2022-11-28 16:15:14 +0530

    Update javascripet.js

--- javascripet.js
+++ javascripet.js
@@ -6,4 +6,13 @@ var re = document.querySelector(':root');
 re.style.setProperty('--ble', bluee);
 re.style.setProperty('--rde', redee);
 re.style.setProperty('--gre', greenee);
+if(document.getElementById('radio-l').checked){
+
+ }
+else if(document.getElementById('radio-r').checked){
+
+  }
+else{
+
+ }
 }
